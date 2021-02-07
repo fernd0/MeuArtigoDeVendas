faea5ce4da4737d559d8269502236ab678573432
Fernando <69485703+fernd0@users.noreply.github.com>
2021-02-07 19:27:11 -0300

    att

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,34 @@
 //     $("#locationJS").html(data.geoplugin_city);
 // });
 
+$.get("https://ssl.geoplugin.net/json.gp?k=36a168c612a12e85", function (resp) {
+    var data = JSON.parse(resp);
+    $("#locationJS").html(data.geoplugin_city);
+});
+
+
+$(document).ready(function() { 
+
+    if (jQuery(window).width() < 480){
+        $('.hero').parallax({
+            imageSrc: 'https://atracaosecreta.com/main/img/bg-top-mobile.png',
+            naturalWidth: 600,
+            naturalHeight: 400
+        });
+    }
+
+    if (jQuery(window).width() > 480){
+        $('.hero').parallax({
+            imageSrc: 'https://atracaosecreta.com/main/img/bg-top.png',
+            naturalWidth: 600,
+            naturalHeight: 400
+        });
+    }
+
+    setTimeout(function() { 
+        $('#button_chechout_hero').show(); 
+        $('#button_chechout_final').show(); 
+ }, 1470  * 1 * 1000); 
+});
+
  //1550
