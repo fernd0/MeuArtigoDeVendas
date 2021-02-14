ada2fe0f2c8883b5d686be7022c844a29dc54396
Fernando <69485703+fernd0@users.noreply.github.com>
2021-02-14 20:49:52 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,6 @@
 //     $("#locationJS").html(data.geoplugin_city);
 // });
 
-$(document).ready(function(){setTimeout(function(){$("#button_chechout_hero").show();$("#button_chechout_final").show()},1470*1*1000)});
+$(document).ready(function(){setTimeout(function(){$("#button_chechout_hero").show();$("#button_chechout_final").show()},5)});
 
  //1550
