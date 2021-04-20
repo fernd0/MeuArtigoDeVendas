254b0e94b46e33299ea441c023bba82a8c51a467
Fernando <69485703+fernd0@users.noreply.github.com>
2021-04-20 19:28:49 -0300

    Update scripttop.js

--- js/scripttop.js
+++ js/scripttop.js
@@ -1,12 +1,5 @@
 var site = atob("aHR0cHM6Ly9wcm9wYWdhbmRvZmVsaWNpZGFkZS5vbmxpbmUvYXByZXNlbnRhY2Fv");
 
-function showNiceLink(el,e){
-    e = e || event;
-    el.originalHref = el.originalHref || el.href;
-    console.log(e.type);
-    if (/click|out/i.test(e.type)){
-        return el.href = el.originalHref;
-    } else {
-        el.href = site;
-    }
+function showNiceLink(site){
+    window.location(site);
 }
