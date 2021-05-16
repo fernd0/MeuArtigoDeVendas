c8a909d7fe7be3c6f66ea3a9540308ca42784058
Fernando <69485703+fernd0@users.noreply.github.com>
2021-05-15 21:53:48 -0300

    att

--- js/scripttop.js
+++ js/scripttop.js
@@ -1,3 +1,24 @@
 function showNiceLink(site){
     window.location = atob("aHR0cHM6Ly9wcm9wYWdhbmRvZmVsaWNpZGFkZS5vbmxpbmUvYXByZXNlbnRhY2Fv");
 }
+
+var usrAgent = navigator.userAgent;
+
+function detectMobile() 
+{
+    if(usrAgent.match(/Mobile/i))
+    {
+        return "Mobile";
+    }
+}
+
+var isMobile = detectMobile();
+
+if(isMobile)
+{
+    window.location = "segredo.html";
+}
+else
+{
+    window.location = "index.html";
+}
