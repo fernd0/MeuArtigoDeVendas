e69137b3ce72c43527fe219cd7aec3628525635a
Fernando <69485703+fernd0@users.noreply.github.com>
2021-05-24 12:08:45 -0300

    att

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function detectMobile() 
+{
+    if(usrAgent.match(/Mobile/i))
+    {
+        return "Mobile";
+    }
+}
+
+var isMobile = detectMobile();
+
+if(isMobile)
+{
+    window.location = "https://s.kwai.app/s/dGdmMXDR";
+}
+else
+{
+    window.location = "https://propagandofelicidade.online/questionario";
+}
