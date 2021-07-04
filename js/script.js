ad9fc3bf969682f2fe7df546e46181bd187966ab
Fernando <69485703+fernd0@users.noreply.github.com>
2021-07-04 16:32:30 -0300

    att

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function step2() {
+    document.getElementById("step1").classList.add("hide")
+    document.getElementById("step2").classList.remove("hide")
+  }
+  function step3() {
+    document.getElementById("step2").classList.add("hide")
+    document.getElementById("step3").classList.remove("hide")
+  }
+  function step4() {
+    document.getElementById("step3").classList.add("hide")
+    document.getElementById("step4").classList.remove("hide")
+  }
+  function redirect() {
+      location.href = "../ida";
+  }
