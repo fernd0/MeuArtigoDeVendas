2a57f55d5676be2a81d37830b0410ff69f3d0f59
Fernando <69485703+fernd0@users.noreply.github.com>
2021-07-04 16:51:24 -0300

    att

--- js/loader.js
+++
@@ -1,9 +0,0 @@
-function load(file)
-{
-    var src = document.createElement('script');
-    src.setAttribute('type', 'text/javascript');
-    src.setAttribute('src', file);
-    document.getElementsByTagName('head')[0].appendChild(src);
-}
-
-load('js/script.js')
