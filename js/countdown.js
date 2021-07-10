0de507e24b2af4aa326469eb7cf3ce571a2bb90b
Fernando <69485703+fernd0@users.noreply.github.com>
2021-07-10 18:26:47 -0300

    att

--- js/countdown.js
+++ js/countdown.js
@@ -1,53 +1,17 @@
-"use strict"
+const startingMinutes = 30;
+let time = startingMinutes * 60;
 
-var hh = 0;
-var mm = 30;
-var ss = 0;
+const countdownEl = document.getElementById('countdown');
 
-var tempo = 1000;//Quantos milésimos valem 1 segundo?
-var cron;
+setInterval(updateCountdown, 1000);
 
-//Inicia o temporizador
-function start() {
-    cron = setInterval(() => { timer(); }, tempo);
+function updateCountdown(){
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+    time = time < 0 ? 0 : time;
 }
-
-//Para o temporizador mas não limpa as variáveis
-function pause() {
-    clearInterval(cron);
-}
-
-//Para o temporizador e limpa as variáveis
-function stop() {
-    clearInterval(cron);
-    hh = 0;
-    mm = 0;
-    ss = 0;
-
-    document.getElementById('counter').innerText = '00:00:00';
-}
-
-//Faz a contagem do tempo e exibição
-function timer() {
-    ss--; //Incrementa +1 na variável ss
-
-    if (ss == 59) { //Verifica se deu 59 segundos
-        ss = 0; //Volta os segundos para 0
-        mm-- //Adiciona +1 na variável mm
-
-        if (mm == 59) { //Verifica se deu 59 minutos
-            mm = 0;//Volta os minutos para 0
-            hh--;//Adiciona +1 na variável hora
-        }
-    }
-
-    //Cria uma variável com o valor tratado HH:MM:SS
-    var format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
-    
-    //Insere o valor tratado no elemento counter
-    document.getElementById('counter').innerText = format;
-
-    //Retorna o valor tratado
-    return format;
-}
-start();
